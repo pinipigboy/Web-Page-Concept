b0e3957df81bb942a93e0b57027247afffcb64ad
pinipigboy <sagan.rauscher@gmail.com>
2024-03-05 15:28:10 -0800

    Home page progress. HTML current focus.

--- FinalSolution/FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalSolution/FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ff7c2cea4e67ab21119f1c8d7b1a5a971e0987d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb230c3cccf52d1743d28be34e25a6dd638d644e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
