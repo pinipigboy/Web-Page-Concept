a5ea6b910775398a9afcded103d7a6f1e1375f2b
pinipigboy <sagan.rauscher@gmail.com>
2024-03-13 18:04:35 -0700

    Testing branch because paranoia

--- FinalSolution/FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalSolution/FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb230c3cccf52d1743d28be34e25a6dd638d644e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56fb52da86440adc481017d36fc424911cd1f65e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
